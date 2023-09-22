52c747f9400b0ee8d0109325101e5d8289db022a
qiming86 <lqm.liqiming@gmail.com>
2023-09-23 00:09:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ fetch('https://qiming86.github.io/questions.json')
 
         imageElement.src = randomImageURL;
 
-		availableNames = data.map(item => item.imageName).filter(name => name !== randomImageName);
-        availableNames = availableNames.sort(() => Math.random() - 0.5).slice(0, 3);
-		availableNames.push(randomImageName).sort(() => Math.random() - 0.5);
-		avilElement.textContent = availableNames;
+		//availableNames = data.map(item => item.imageName).filter(name => name !== randomImageName);
+        //availableNames = availableNames.sort(() => Math.random() - 0.5).slice(0, 3);
+		//availableNames.push(randomImageName).sort(() => Math.random() - 0.5);
+		//avilElement.textContent = availableNames;
 		
 
         for (let i = 0; i < buttons.length; i++) {
